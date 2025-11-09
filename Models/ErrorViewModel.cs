c2801d0d8a20c2d4732aaa5e565c8b7c7b76b31f
trustoriginalityai <trustoriginalityai@hotmail.com>
2025-11-09 15:33:51 +0300

    Add new files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TrustOriginalityAI_Prod.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
